4585a5b5c0a28348ec40a55a89d2fd8ead6ff168
Charalampos <charis.s.sideris@gmail.com>
2024-10-12 20:01:19 +0300

    I opened a new branch and I made it!!!

--- add_function.py
+++ add_function.py
@@ -1,2 +1,13 @@
-x=4
-print(x)
+#This code asks for 2 numbers and calculate the sum
+
+# Ask the user for the first number
+num1 = float(input("Enter the first number: "))
+
+# Ask the user for the second number
+num2 = float(input("Enter the second number: "))
+
+# Calculate the sum of the two numbers
+sum = num1 + num2
+
+# Display the result
+print(f"The sum of {num1} and {num2} is {sum}.")
